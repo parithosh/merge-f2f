083fc8451ab763d0f3e37fa58bf09c765452956c
parithosh <parithosh.jayanthi@ethereum.org>
2021-10-03 23:48:28 +0300

    geth cleanup and added vars for geth

---
+++ m2/execution-clients/geth/start_geth.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+. ./vars.env
+
+./go-ethereum/build/bin/geth --catalyst --datadir "$DATA_DIR" init $GENESIS_JSON_LOCATION
+./go-ethereum/build/bin/geth --catalyst --http --ws -http.api "engine,eth" --datadir "$DATA_DIR"
