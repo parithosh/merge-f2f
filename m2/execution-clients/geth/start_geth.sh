87b23ae62bd6ce77ee01c8fbffb1348d8146950e
parithosh <parithosh.jayanthi@ethereum.org>
2021-10-04 10:29:35 +0300

    removed geth command

--- m2/execution-clients/geth/start_geth.sh
+++ m2/execution-clients/geth/start_geth.sh
@@ -3,4 +3,4 @@
 . ./vars.env
 
 ./go-ethereum/build/bin/geth --catalyst --datadir "$DATA_DIR" init $GENESIS_JSON_LOCATION
-./go-ethereum/build/bin/geth --catalyst --http --ws -http.api "engine,eth" --datadir "$DATA_DIR"
+#./go-ethereum/build/bin/geth --catalyst --http --ws -http.api "engine,eth" --datadir "$DATA_DIR"
