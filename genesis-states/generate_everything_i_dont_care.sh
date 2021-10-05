18ccf6ffeee83b15365d0c32a33e24b6d8cab4d4
parithosh <parithosh.jayanthi@ethereum.org>
2021-10-05 11:48:18 +0300

    modified docker and added deposit contract address

--- genesis-states/generate_everything_i_dont_care.sh
+++ genesis-states/generate_everything_i_dont_care.sh
@@ -20,6 +20,9 @@ python generate_eth2_conf.py > "$TESTNET_NAME/public/eth2_config.yaml"
 # generate eth2 keys and state
 ./generate_eth2_validator_data.sh
 
+DEPOSIT_CONTRACT_ADDRESS="$(grep "^deposit_contract_address" mergenet.yaml | awk -F": " '{ print $2 }')"
 # create extra files needed by eth2 clients
 echo 0 > "$TESTNET_NAME/public/deploy_block.txt"
+echo $DEPOSIT_CONTRACT_ADDRESS > "$TESTNET_NAME/public/deposit_contract.txt"
+
 cp "$TESTNET_NAME/public/eth2_config.yaml" "$TESTNET_NAME/public/config.yaml"
